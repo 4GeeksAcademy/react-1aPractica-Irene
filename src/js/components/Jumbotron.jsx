1933519b20036a7f2d0506aace0894076d634b95
Irenecia <irenecia77@gmail.com>
2025-12-03 06:58:00 +0000

    trabajo de react

---
+++ src/js/components/Jumbotron.jsx
@@ -0,0 +1,15 @@
+export default function Jumbotron () {  
+return (
+
+<div className="jumbotron p-5">
+      <h1 className="display-4">A Warm Welcome!</h1>
+      <p className="lead">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim 
+        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+        aliquip ex ea commodo consequat.
+      </p>
+      <hr className="my-4" />
+    </div>
+  );
+}
